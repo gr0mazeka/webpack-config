8de4b1a1fae3a9d882411544ec186b2f7b04595f
gr0mazeka <kmvpipl@yandex.com>
2021-05-12 10:32:24 +0300

    add react support

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,47 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-let mode = "development";
-let target = "web";
+let mode = 'development';
+let target = 'web';
 
-if (process.env.NODE_ENV === "production") {
-  mode = "production";
-  target = "browserslist";
+if (process.env.NODE_ENV === 'production') {
+    mode = 'production';
+    target = 'browserslist';
 }
 
 module.exports = {
-  mode: mode,
-  target: target,
+    mode: mode,
+    target: target,
 
-  module: {
-    rules: [
-      {
-        test: /\.(s[ac]|c)ss$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader",
-          "sass-loader",
+    module: {
+        rules: [
+            {
+                test: /\.(s[ac]|c)ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
         ],
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-    ],
-  },
+    },
 
-  plugins: [new MiniCssExtractPlugin()],
+    plugins: [new MiniCssExtractPlugin()],
 
-  devtool: "source-map",
-  devServer: {
-    contentBase: "./dist",
-    hot: true,
-  },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+
+    devtool: 'source-map',
+    devServer: {
+        contentBase: './dist',
+        hot: true,
+    },
 };
